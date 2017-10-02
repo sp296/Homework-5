fdeba0da98e7afc2a2631e402981b728457991f8
sp296 <32467180+sp296@users.noreply.github.com>
2017-10-02 17:24:21 -0400

    changes

---
+++ Assets/KeyboardCameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardCameraController : MonoBehaviour {
+
+	public float rotationSpeed=50.0f;
+	public float translationSpeed=20.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey ("left")) 
+			this.transform.Rotate (0f, -1.0f * rotationSpeed * Time.deltaTime,0f);
+		
+		if (Input.GetKey ("right")) 
+			this.transform.Rotate (0f, rotationSpeed * Time.deltaTime,0f);
+		
+		if (Input.GetKey("up"))
+			this.transform.Translate (Vector3.forward * Time.deltaTime * translationSpeed);  
+		
+		if (Input.GetKey("down"))
+			this.transform.Translate (Vector3.forward * Time.deltaTime * translationSpeed * -1.0f);  
+		
+	}
+}
---
+++ Assets/MoveToSign.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToSign : MonoBehaviour {
+	float elapsedTime=0.0f;
+	Vector3 startPos=new Vector3(0,0,0);
+	Vector3 initialPos = new Vector3 (0, 0, 0);
+	Vector3 endPos =new Vector3(50,10,190);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("a")) {
+			while (initialPos != endPos) {
+				elapsedTime += Time.deltaTime;
+				float percentComplete = elapsedTime / 3.0f; //go from 0.0 to 1.0
+				transform.position = Vector3.Lerp (startPos, endPos, percentComplete);
+			}
+		}
+	}
+}
